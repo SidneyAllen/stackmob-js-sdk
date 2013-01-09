eed5c12d2761ecd24af952888a5fe8cd14499fc0
Erick Tai <erick@stackmob.com>
2013-01-09 09:53:22 -0800

    Adding camelCase custom method tests. Also refactored test generation method for reuse.

--- test/spec/CustomCodeOAuthSpec.js
+++ test/spec/CustomCodeOAuthSpec.js
@@ -1,10 +1,12 @@
 describe("Custom Code OAuth Signing Tests", function() {
   
-  var makeLegitOAuthCustomCodeCall = function(verb) {
-    it("should recognize the logged in user in custom code " + verb + " requests", function() {
+  var userpass = 'ccuser';
+  
+  var customCodeCall = function(verb, method, shouldMsg, check) {
+    it(shouldMsg, function() {
       var response = null;
       
-      StackMob.customcode('hello_world', {}, verb, {
+      StackMob.customcode(method, {}, verb, {
         success: function(result) {
           response = result;
         }
@@ -14,33 +16,29 @@ describe("Custom Code OAuth Signing Tests", function() {
         return response;
       }, "to get result back from custom code", 20000);
       
-      runs(function() {
-        expect(response['msg']).toMatch(userpass)
-      });
-    });    
+      runs(function(response) { check(response); });
+    });
+  }
+  
+  var makeLegitOAuthCustomCodeCall = function(verb) {
+    customCodeCall(verb, 'hello_world', "should recognize the logged in user in custom code " + verb + " requests", function(response) {
+        return function() { expect(response['msg']).toMatch(userpass); }
+    });
   }
   
   var makeIncorrectOAuthCustomCodeCall = function(verb) {
-    it("should NOT recognize a logged out user in custom code " + verb + " requests", function() {
-      var response = null;
-      
-      StackMob.customcode('hello_world', {}, verb, {
-        success: function(result) {
-          response = result;
-        }
-      });
-      
-      waitsFor(function() {
-        return response;
-      }, "to get result back from custom code", 20000);
-      
-      runs(function() {
-        expect(response['msg']).toBeNull();
-      });
-    });    
+    customCodeCall(verb, 'hello_world', "should NOT recognize a logged out user in custom code " + verb + " requests", function(response) {
+      return function() { expect(response['msg']).toBeNull(); }
+    });
   }
   
-  var userpass = 'ccuser';
+  var makeCamelCaseCall = function(verb, method) {
+    customCodeCall(verb, method, "should recognize the logged in user for camelcase custom code " + verb + " requests", function(response) {
+      return function() { expect(response['msg']).toMatch(userpass); }
+    });
+  }
+  
+  
   
   createSimpleUser(userpass);
   loginUser(userpass);
@@ -49,6 +47,24 @@ describe("Custom Code OAuth Signing Tests", function() {
   makeLegitOAuthCustomCodeCall('POST');
   makeLegitOAuthCustomCodeCall('PUT');
   makeLegitOAuthCustomCodeCall('DELETE');
+  
+  //UPPERCASE
+  makeCamelCaseCall('GET', 'CAMELCASE');
+  makeCamelCaseCall('POST', 'CAMELCASE');
+  makeCamelCaseCall('PUT', 'CAMELCASE');
+  makeCamelCaseCall('DELETE', 'CAMELCASE');
+  
+  //camelCase
+  makeCamelCaseCall('GET', 'camelCase');
+  makeCamelCaseCall('POST', 'camelCase');
+  makeCamelCaseCall('PUT', 'camelCase');
+  makeCamelCaseCall('DELETE', 'camelCase');
+
+  //lowercase
+  makeCamelCaseCall('GET', 'camelcase');
+  makeCamelCaseCall('POST', 'camelcase');
+  makeCamelCaseCall('PUT', 'camelcase');
+  makeCamelCaseCall('DELETE', 'camelcase');  
   
   logoutUser(userpass);
   
