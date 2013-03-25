604743f26339729bfbff37c1936e12e9f61f5c13
Justin Wyne <justin@stackmob.com>
2013-03-25 15:27:57 -0700

    Unit test for unlink Gigya

--- test/spec/unit/SocialSpec.js
+++ test/spec/unit/SocialSpec.js
@@ -4,21 +4,40 @@
 
 describe("Social Unit Tests", function() {
 
+  function testEndPoint(obj, sdkMethod, verb, endPoint) {
+    var calledBack = false;
+    obj[sdkMethod]({
+      done: function(model, params, method) {
+        calledBack = true;
+        expect(params['url'].indexOf(endPoint) > -1).toEqual(true);
+        expect(params['type']).toEqual(verb);
+      }
+    });
+    expect(calledBack).toEqual(true);
+  }
+
   it("should call unlinkUserFromFacebook", function() {
 
     runs(function() {
       var model, params, method;
 
       var user = new StackMob.User({username: "testUser", password: "testUser"});
-      user.unlinkUserFromFacebook({
-        done: function(mod,p,m){
-          model = mod;
-          params = p;
-          method = m;
-        }
-      });
 
-      expect(params['url'].indexOf('user/unlinkUserFromFacebook') > -1).toEqual(true);
+      testEndPoint(user, 'unlinkUserFromFacebook', 'DELETE', 'user/unlinkUserFromFacebook');
+
+    });
+
+  });
+
+  it("should call unlinkUserFromGigya", function() {
+
+    runs(function() {
+      var model, params, method;
+
+      var user = new StackMob.User({username: "testUser", password: "testUser"});
+
+      testEndPoint(user, 'unlinkUserFromGigya', 'DELETE', 'user/unlinkUserFromGigya');
+
     });
 
   });
