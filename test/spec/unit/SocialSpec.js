f735b11d21b42b9d00794ce01b03074d82a605d5
ardok <ardo@stackmob.com>
2013-04-02 11:47:20 -0700

    Card 605 add unit tests

--- test/spec/unit/SocialSpec.js
+++ test/spec/unit/SocialSpec.js
@@ -1,5 +1,12 @@
 /**
  * Social Unit Tests
+ *
+ * expectEndpoint params:
+ * 1) user object
+ * 2) array of params without `options`
+ * 3) method name
+ * 4) HTTP verb
+ * 5) Endpoint
  */
 
 describe("Social Unit Tests", function() {
@@ -12,19 +19,6 @@ describe("Social Unit Tests", function() {
       var user = new StackMob.User({username: "testUser", password: "testUser"});
 
       expectEndpoint(user, [], 'unlinkUserFromFacebook', 'DELETE', 'user/unlinkUserFromFacebook');
-
-    });
-
-  });
-
-  it("should call unlinkUserFromGigya", function() {
-
-    runs(function() {
-      var model, params, method;
-
-      var user = new StackMob.User({username: "testUser", password: "testUser"});
-
-      expectEndpoint(user, [], 'unlinkUserFromGigya', 'DELETE', 'user/unlinkUserFromGigya');
 
     });
 
@@ -52,6 +46,36 @@ describe("Social Unit Tests", function() {
 
       expectEndpoint(user, ['', false],'loginWithFacebookAutoCreate', 'POST', 'user/facebookAccessTokenWithCreate');
 
+    });
+
+  });
+
+  it("should call gigyaAccessToken", function() {
+
+    runs(function() {
+      var user = new StackMob.User({ username: "testUser", password: "testUser" });
+
+      expectEndpoint(user, ['', '', '', false], 'gigyaAccessToken', 'POST', 'user/gigyaAccessToken')
+    });
+
+  });
+
+  it("should call linkUserWithGigya", function() {
+
+    runs(function() {
+      var user = new StackMob.User({ username: "testUser", password: "testUser" });
+
+      expectEndpoint(user, ['', '', ''], 'linkUserWithGigya', 'POST', 'user/linkUserWithGigya');
+    });
+
+  });
+
+  it("should call unlinkUserFromGigya", function() {
+
+    runs(function() {
+      var user = new StackMob.User({ username: "testUser", password: "testUser" });
+
+      expectEndpoint(user, [], 'unlinkUserFromGigya', 'DELETE', 'user/unlinkUserFromGigya');
     });
 
   });
